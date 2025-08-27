3a3b1367fb47c230717b232153a96cd849dc0321
Dimitri Lerévérend <dimitri.lereverend@inria.fr>
2025-08-27 18:27:13 +0200

    Added two decentralized workloads

--- simulations/workloads_generator.py
+++ simulations/workloads_generator.py
@@ -1,5 +1,7 @@
 import numpy as np
+import optimal_factorization
 import torch
+import utils
 from scipy.linalg import toeplitz
 
 
@@ -17,23 +19,20 @@ def get_pi(nb_nodes, nb_iterations):
     return permutation
 
 
-def build_participation_matrix(num_epochs: int, participation_interval: int):
-    assert num_epochs % participation_interval == 0
-    m = np.zeros(shape=(num_epochs, participation_interval))
+def build_participation_matrix(nb_steps: int, participation_interval: int):
+    assert nb_steps % participation_interval == 0
+    m = np.zeros(shape=(nb_steps, participation_interval))
 
     # Single participation
-    if participation_interval >= num_epochs:
+    if participation_interval >= nb_steps:
         print("Found single participation setting")  # TODO: remove debug print
-        for i in range(num_epochs):
+        for i in range(nb_steps):
             m[i, i] = 1
         return m
 
     # Multiple round participation
     row_indexes = np.array(
-        [
-            participation_interval * i
-            for i in range(num_epochs // participation_interval)
-        ]
+        [participation_interval * i for i in range(nb_steps // participation_interval)]
     )
     for i in range(participation_interval):
         m[row_indexes + i, i] = 1
@@ -41,7 +40,7 @@ def build_participation_matrix(num_epochs: int, participation_interval: int):
 
 
 def compute_sensitivity(
-    C: np.ndarray, participation_interval: int, num_epochs: int
+    C: np.ndarray, participation_interval: int, nb_steps: int
 ) -> float:
     # Check if C is the zero matrix
     if np.allclose(C, 0):
@@ -49,11 +48,26 @@ def compute_sensitivity(
 
     X = C.T @ C
 
+    assert (
+        nb_steps % participation_interval == 0
+    ), f"Participation Interval {participation_interval} does not divide number of steps {nb_steps}"
+
+    participation_mask = utils.get_orthogonal_mask(n=nb_steps, epochs=nb_steps)
+
     if np.all(X >= 0):
+        # Using the trick of Corollary 2.1 (https://proceedings.mlr.press/v202/choquette-choo23a/choquette-choo23a.pdf)
         contrib_matrix = build_participation_matrix(
-            num_epochs=num_epochs, participation_interval=participation_interval
+            nb_steps=nb_steps, participation_interval=participation_interval
         )
         sens = np.sqrt(np.max(np.diag(contrib_matrix.T @ X @ contrib_matrix)))
+
+    elif np.all((1 - participation_mask) * X == 0):
+        diag = np.diag(X)
+        sensitivities = []
+        for i in range(participation_interval):
+            idx = np.arange(i, nb_steps, participation_interval)
+            sensitivities.append(np.sqrt(np.sum(diag[idx] ** 2)))
+        sens = np.max(np.array(sensitivities))
 
     else:
         raise NotImplementedError("Negative matrix factorization")
@@ -96,6 +110,68 @@ def build_DL_workload(
             np.linalg.matrix_power(matrix, i + initial_power),
         )
     return time_matrix
+
+
+def build_local_DL_workload(matrix: np.ndarray, nb_steps: int, initial_power: int = 0):
+    """Builds the local version of the DL workload, where each node will have the same local correlation.
+    This is the workload that will be optimized in order to obtain an optimal C such that A = B @ pi@ np.kron(In, C)
+
+    Args:
+        matrix (np.ndarray): The graph matrix
+        nb_steps (int): The number of iterations of DL (and communication rounds)
+        initial_power (int, default 0): Initial power of the workload matrix.
+            Defines what power of matrix is in the diagonal.
+            1 is the matrix itself (optimization workload), 0 is Id (privacy workload).
+    """
+    nb_nodes = matrix.shape[0]
+
+    dl_workload = build_DL_workload(
+        matrix, nb_steps=nb_steps, initial_power=initial_power
+    )  # nT * nT
+    pi = get_pi(nb_nodes=nb_nodes, nb_iterations=nb_steps)  # nT * Tn
+    A = dl_workload @ pi
+
+    gram_workload = np.zeros((nb_steps, nb_steps))
+    for i in range(nb_nodes):
+        Ai = A[:, nb_steps * i : nb_steps * (i + 1)]
+        gram_workload += Ai.T @ Ai
+
+    # TODO: remove this
+    is_positive_definite = utils.check_positive_definite(gram_workload)
+
+    if is_positive_definite:
+        surrogate_workload = np.linalg.cholesky(gram_workload)
+    else:
+        print(
+            "!" * 50
+            + "Warning - DL surrogate workload is not definite positive, trying an experimental surrogate workload that may make optimization fail."
+            + "!" * 50
+        )
+        # Compute square root using - but problem if this workload is not full rank?
+        eigvals, eigvecs = np.linalg.eigh(gram_workload)
+        # Set negative eigenvalues to zero (for numerical stability)
+        eigvals[eigvals < 0] = 0
+        surrogate_workload = eigvecs @ np.diag(np.sqrt(eigvals))
+
+    return surrogate_workload
+
+
+def MF_OPTIMAL_DL(communication_matrix, nb_nodes, nb_steps, nb_epochs):
+    surrogate_workload = build_local_DL_workload(
+        communication_matrix, nb_steps=nb_steps
+    )
+    B_optimal, C_optimal = optimal_factorization.get_optimal_factorization(
+        surrogate_workload, nb_steps=nb_steps, nb_epochs=nb_epochs
+    )
+    return B_optimal, C_optimal
+
+
+def MF_OPTIMAL_local(communication_matrix, nb_nodes, nb_steps, nb_epochs):
+    centralized_workload = np.tri(nb_steps)
+    B_optimal, C_optimal = optimal_factorization.get_optimal_factorization(
+        centralized_workload, nb_steps=nb_steps, nb_epochs=nb_epochs
+    )
+    return B_optimal, C_optimal
 
 
 def space_to_time_permutation_matrix(nb_steps: int, nb_nodes: int):
