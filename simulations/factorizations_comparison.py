Added caching of workloads, added ego graph, fixed incorrect batch size computation in housing experiments.

simulations/factorizations_comparison.py
@@ -11,6 +11,7 @@ import plotters
 import scienceplots
 import utils
 import workloads_generator
+from utils import GraphName
 
 FactorizationName = Literal[
     "LDP",
@@ -37,6 +38,8 @@ def workload_wrapper(
     nb_nodes,
     nb_iterations,
     nb_epochs,
+    graph_name=None,
+    seed=None,
 ):
     match workload_name:
         case "LDP":
@@ -52,31 +55,32 @@ def workload_wrapper(
                 nb_iterations=nb_iterations
             )
         case "OPTIMAL_LOCAL":
-            _, C = workloads_generator.MF_OPTIMAL_local(
+            return workloads_generator.MF_OPTIMAL_local(
                 communication_matrix=communication_matrix,  # Unused argument
                 nb_nodes=1,  # Unused argument
                 nb_steps=nb_iterations,
                 nb_epochs=nb_epochs,
             )
-            return C
         case "OPTIMAL_DL_MSG":
-            _, C = workloads_generator.MF_OPTIMAL_DL(
+            return workloads_generator.MF_OPTIMAL_DL(
                 communication_matrix=communication_matrix,
                 nb_nodes=nb_nodes,
                 nb_steps=nb_iterations,
                 nb_epochs=nb_epochs,
                 post_average=False,
+                graph_name=graph_name,
+                seed=seed,
             )
-            return C
         case "OPTIMAL_DL_POSTAVG":
-            _, C = workloads_generator.MF_OPTIMAL_DL(
+            return workloads_generator.MF_OPTIMAL_DL(
                 communication_matrix=communication_matrix,
                 nb_nodes=nb_nodes,
                 nb_steps=nb_iterations,
                 nb_epochs=nb_epochs,
                 post_average=True,
+                graph_name=graph_name,
+                seed=seed,
             )
-            return C
         case _:
             raise NotImplementedError(
                 f"Did not find a workload strategy {workload_name}"
@@ -97,7 +101,7 @@ def evaluate_loss(gram_workload, C, participation_interval, nb_iterations):
 
 
 def run_experiment(
-    graph_name: str,
+    graph_name: GraphName,
     nb_nodes: int,
     nb_repetition: int,
     participation_interval: int,
@@ -115,8 +119,12 @@ def run_experiment(
     np.random.seed(seed)
 
     # Setup
-    G = utils.get_graph(graph_name, nb_nodes)
-    assert nb_nodes == G.number_of_nodes(), "The number of node was changed"
+    G = utils.get_graph(graph_name, nb_nodes, seed=seed)
+    if nb_nodes != G.number_of_nodes():
+        assert (
+            graph_name == "florentine"
+        ), "The number of node was changed on a non-florentine graph!"
+        nb_nodes = G.number_of_nodes()
     communication_matrix = utils.get_communication_matrix(G)
 
     nb_steps = nb_repetition * participation_interval
@@ -140,6 +148,8 @@ def run_experiment(
             nb_nodes=nb_nodes,
             nb_iterations=nb_steps,
             nb_epochs=nb_repetition,
+            graph_name=graph_name,
+            seed=seed,
         )
         elapsed_time = time.time() - start_time
         # plotters.plot_factorization(np.linalg.pinv(C), factorization_name + "+ $C^+$")

simulations/housing.py
@@ -23,7 +23,7 @@ from sklearn.datasets import fetch_california_housing
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from utils import get_communication_matrix, get_graph
+from utils import GraphName, get_communication_matrix, get_graph
 from workloads_generator import compute_sensitivity
 
 # Remove warnings for Housing that should be safe, raised because of Opacus + Housing
@@ -46,7 +46,7 @@ class HousingMLP(nn.Module):
 
 def learning_step(
     models: list,
-    nb_micro_batches: int,
+    nb_micro_batches_per_step: int,
     optimizers: list,
     data_iters: list,
     dataloaders: list,
@@ -56,13 +56,12 @@ def learning_step(
     # Local update
     for node, model in enumerate(models):
         optimizer: MFDLSGD = optimizers[node]
-        for _ in range(nb_micro_batches):
+        for _ in range(nb_micro_batches_per_step):
             try:
                 batch = next(data_iters[node])
             except StopIteration:
-                data_iters[node] = iter(
-                    dataloaders[node]
-                )  # Reset the current dataloader.
+                # Reset the current dataloader.
+                data_iters[node] = iter(dataloaders[node])
                 batch = next(data_iters[node])
                 if node == 0:
                     nb_resets += 1
@@ -111,7 +110,7 @@ def run_decentralized_training(
     num_steps: int,
     C: np.ndarray,
     epsilon: float,
-    micro_batches_per_epoch: int,
+    micro_batches_per_step: int,
     trainloaders: list[data.DataLoader],
     testloader: data.DataLoader,
     input_dim: int,
@@ -128,7 +127,7 @@ def run_decentralized_training(
         model = GradSampleModule(HousingMLP(input_dim)).to(device)
         models.append(model)
     participations_intervals = [
-        len(trainloader) // micro_batches_per_epoch for trainloader in trainloaders
+        len(trainloader) // micro_batches_per_step for trainloader in trainloaders
     ]
     # optimizers = [optim.Adam(models[i].parameters(), lr=1e-2) for i in range(num_nodes)]
     optimizers = []
@@ -144,7 +143,7 @@ def run_decentralized_training(
                 device=device,
                 id=i,
                 noise_multiplier=1 / epsilon,
-                batch_size=batch_size * micro_batches_per_epoch,
+                batch_size=batch_size * micro_batches_per_step,
             )
         )
     data_iters = [iter(trainloaders[i]) for i in range(num_nodes)]
@@ -155,7 +154,7 @@ def run_decentralized_training(
         print(f"Step {step + 1:>{len(str(num_steps))}}/{num_steps}", end="\r")
         nb_resets += learning_step(
             models,
-            nb_micro_batches=micro_batches_per_epoch,
+            nb_micro_batches_per_step=micro_batches_per_step,
             optimizers=optimizers,
             data_iters=data_iters,
             dataloaders=trainloaders,
@@ -265,7 +264,7 @@ def run_simulation(
     args,
     G: nx.Graph,
     num_steps,
-    micro_batches_per_epoch,
+    micro_batches_per_step,
     epsilon: float,
     input_dim,
     device_name: str,
@@ -285,7 +284,7 @@ def run_simulation(
     print(f"Running simulation for {name} (PID: {os.getpid()})")
     sens = compute_sensitivity(
         C,
-        participation_interval=nb_micro_batches // micro_batches_per_epoch,
+        participation_interval=nb_micro_batches // micro_batches_per_step,
         nb_steps=num_steps,
     )
     print(f"sens²(C_{name}) = {sens**2}")
@@ -296,7 +295,7 @@ def run_simulation(
         num_steps=num_steps,
         C=C,
         epsilon=epsilon,
-        micro_batches_per_epoch=micro_batches_per_epoch,
+        micro_batches_per_step=micro_batches_per_step,
         trainloaders=trainloaders,
         testloader=testloader,
         input_dim=input_dim,
@@ -312,27 +311,29 @@ def run_experiment(
     debug=True,
     use_optimals=False,
     nb_nodes=10,
-    graph_name="expander",
+    graph_name: GraphName = "expander",
     epsilon: float = 1.0,
     num_repetition=5,
     dataloader_seed=421,
     lr=1e-1,
-    nb_micro_batches=16,
+    nb_big_batches=16,
     recompute: bool = False,
+    pre_cache: bool = False,
 ):
     device_name = "cpu"
     device = torch.device(device_name)
     print(f"Device : {device}")
 
-    # 20,640 total samples, 20,640 * 0.8 = 16,512 train samples
-    micro_batches_size = 16512 // (nb_nodes * nb_micro_batches)
-    micro_batches_per_epoch = 1
-
-    input_dim = 8  # California housing dataset
-
-    G = get_graph(graph_name, nb_nodes)
+    G = get_graph(graph_name, nb_nodes, seed=dataloader_seed)
     nb_nodes = G.number_of_nodes()
     communication_matrix = get_communication_matrix(G)
+
+    # 20,640 total samples, 20,640 * 0.8 = 16,512 train samples
+    micro_batches_per_step = 1
+    micro_batches_per_epoch = nb_big_batches * micro_batches_per_step
+    micro_batches_size = 16512 // (nb_nodes * micro_batches_per_epoch)
+
+    input_dim = 8  # California housing dataset
 
     trainloaders, _ = load_housing(nb_nodes, train_batch_size=micro_batches_size)
     nb_micro_batches_list = [len(loader) for loader in trainloaders]
@@ -340,7 +341,7 @@ def run_experiment(
     assert np.all(np.array(nb_micro_batches_list) == np.min(nb_micro_batches_list))
     nb_micro_batches_val = np.min(nb_micro_batches_list)
 
-    num_steps = num_repetition * (nb_micro_batches_val // micro_batches_per_epoch)
+    num_steps = num_repetition * (nb_micro_batches_val // micro_batches_per_step)
 
     print(f"Number of steps per epoch: {num_steps // num_repetition}")
 
@@ -350,7 +351,8 @@ def run_experiment(
         f"nodes{nb_nodes}_"
         f"eps{epsilon}_"
         f"reps{num_repetition}_"
-        f"mbpe{micro_batches_per_epoch}_"
+        f"nbbatches{nb_big_batches}_"
+        f"mbps{micro_batches_per_step}_"
         f"lr{lr}_"
         f"seed{dataloader_seed}_"
         f"mbsize{micro_batches_size}_"
@@ -359,10 +361,10 @@ def run_experiment(
     details = (
         f"Graph: {graph_name} | "
         f"Nb nodes: {nb_nodes} | "
-        f"Micro-batches per epoch: {micro_batches_per_epoch} | ",
-        f"Nb micro batches: {nb_micro_batches} | "
+        f"Micro-batches per step: {micro_batches_per_step} | "
+        f"Nb big batches: {nb_big_batches} | "
         f"Micro-batch size: {micro_batches_size} | "
-        f"Num_repetitions: {num_repetition}",
+        f"Num_repetitions: {num_repetition}"
     )
     csv_path = f"results/housing/simulation_{current_experiment_properties}.csv"
 
@@ -389,24 +391,47 @@ def run_experiment(
     if use_optimals:
         print("Starting computation of C_OPTIMAL_LOCAL...")
         start_time_local = time.time()
-        _, C_OPTIMAL_LOCAL = workloads_generator.MF_OPTIMAL_local(
+        C_OPTIMAL_LOCAL = workloads_generator.MF_OPTIMAL_local(
             communication_matrix=communication_matrix,
             nb_nodes=nb_nodes,
             nb_steps=num_steps,
             nb_epochs=num_repetition,
+            caching=True,
+            verbose=True,
         )
         elapsed_time_local = time.time() - start_time_local
         print(
             f"Finished computation of C_OPTIMAL_LOCAL in {elapsed_time_local:.2f} seconds."
         )
 
-        print("Starting computation of C_OPTIMAL_DL...")
+        print("Starting computation of C_OPTIMAL_MSG...")
         start_time_dl = time.time()
-        _, C_OPTIMAL_DL = workloads_generator.MF_OPTIMAL_DL(
+        C_OPTIMAL_DL = workloads_generator.MF_OPTIMAL_DL(
             communication_matrix=communication_matrix,
             nb_nodes=nb_nodes,
             nb_steps=num_steps,
             nb_epochs=num_repetition,
+            post_average=False,
+            graph_name=graph_name,
+            seed=dataloader_seed,
+            caching=True,
+            verbose=True,
+        )
+        elapsed_time_dl = time.time() - start_time_dl
+        print(f"Finished computation of C_OPTIMAL_DL in {elapsed_time_dl:.2f} seconds.")
+
+        print("Starting computation of C_OPTIMAL_DL...")
+        start_time_dl = time.time()
+        C_OPTIMAL_DL_POSTAVERAGE = workloads_generator.MF_OPTIMAL_DL(
+            communication_matrix=communication_matrix,
+            nb_nodes=nb_nodes,
+            nb_steps=num_steps,
+            nb_epochs=num_repetition,
+            post_average=True,
+            graph_name=graph_name,
+            seed=dataloader_seed,
+            caching=True,
+            verbose=True,
         )
         elapsed_time_dl = time.time() - start_time_dl
         print(f"Finished computation of C_OPTIMAL_DL in {elapsed_time_dl:.2f} seconds.")
@@ -416,6 +441,13 @@ def run_experiment(
             title="C Optimal DL workload",
             details=details,
             save_name_properties=f"DLopti_{current_experiment_properties}",
+            debug=debug,
+        )
+        plotters.plot_factorization(
+            C_OPTIMAL_DL_POSTAVERAGE,
+            title="C optimal DL averaged workload",
+            details=details,
+            save_name_properties=f"DLAVGopti_{current_experiment_properties}",
             debug=debug,
         )
         plotters.plot_factorization(
@@ -437,14 +469,24 @@ def run_experiment(
             participation_interval=num_steps // num_repetition,
             nb_steps=num_steps,
         )
-        configs.append(("OPTIMAL", C_OPTIMAL_DL))
+        compute_sensitivity(
+            C_OPTIMAL_DL_POSTAVERAGE,
+            participation_interval=num_steps // num_repetition,
+            nb_steps=num_steps,
+        )
+        configs.append(("OPTIMAL_DL_MSG", C_OPTIMAL_DL))
+        configs.append(("OPTIMAL_DL_POSTAVG", C_OPTIMAL_DL_POSTAVERAGE))
         configs.append(("OPTIMAL_LOCAL", C_OPTIMAL_LOCAL))
+
+    if pre_cache:
+        # Don't run simulations, useful for servers where factorization is quick but simulation slow.
+        return
 
     # Use ProcessPoolExecutor for true parallelism with PyTorch
     run_sim_args = {
         "G": G,
         "num_steps": num_steps,
-        "micro_batches_per_epoch": micro_batches_per_epoch,
+        "micro_batches_per_step": micro_batches_per_step,
         "epsilon": epsilon,
         "input_dim": input_dim,
         "device_name": device_name,
@@ -489,8 +531,9 @@ def run_experiment(
     df["num_nodes"] = nb_nodes
     df["num_repetitions"] = num_repetition
     df["micro_batch_size"] = micro_batches_size
-    df["micro_batches_per_epoch"] = micro_batches_per_epoch
+    df["micro_batches_per_step"] = micro_batches_per_step
     df["nb_micro_batches"] = nb_micro_batches_val
+    df["nb_big_batches"] = nb_big_batches
     df["input_dim"] = input_dim
     df["device"] = device_name
     df["dataloader_seed"] = run_sim_args["dataloader_seed"]
@@ -535,10 +578,10 @@ def main():
     )
     parser.add_argument("--lr", type=float, default=1e-1, help="Learning rate")
     parser.add_argument(
-        "--nb_micro_batches",
+        "--nb_batches",
         type=int,
         default=16,
-        help="Number of micro-batches per node (default: 16)",
+        help="Number of (big) batches per node (default: 16)",
     )
     parser.add_argument(
         "--debug",
@@ -552,6 +595,12 @@ def main():
         default=False,
         help="Restart all experiments from scratch. Will ignore already-computed csv files, disabling checkpointing.",
     )
+    parser.add_argument(
+        "--pre_cache",
+        action="store_true",
+        default=False,
+        help="Only compute the necessary matrix factorizations, and make sure they are in the cache for later simulations.",
+    )
 
     args = parser.parse_args()
 
@@ -563,9 +612,10 @@ def main():
         epsilon=args.epsilon,
         dataloader_seed=args.dataloader_seed,
         lr=args.lr,
-        nb_micro_batches=args.nb_micro_batches,
+        nb_big_batches=args.nb_batches,
         debug=args.debug,
         recompute=args.recompute,
+        pre_cache=args.pre_cache,
     )
 
 

simulations/optimal_factorization.py
@@ -188,17 +188,23 @@ class MatrixFactorizer:
         # C_lower = np.linalg.cholesky(gram_matrix)
         # C_matrix = C_lower.T.astype(self.workload_matrix.dtype)
 
-        B_matrix = self.workload_matrix @ np.linalg.pinv(C_matrix)
-        return B_matrix, C_matrix
+        # B_matrix = self.workload_matrix @ np.linalg.pinv(C_matrix)
+        return C_matrix
 
 
 def get_optimal_factorization(
-    workload, nb_steps: int, nb_epochs: int, optimizer_iterations: int = 10000
-):
-    optimizer = MatrixFactorizer(workload_matrix=workload, nb_epochs=nb_epochs)
+    workload,
+    nb_steps: int,
+    nb_epochs: int,
+    optimizer_iterations: int = 10000,
+    verbose: bool = False,
+) -> np.ndarray:
+    optimizer = MatrixFactorizer(
+        workload_matrix=workload, nb_epochs=nb_epochs, verbose=verbose
+    )
     gram_encoder = optimizer.optimize(optimizer_iterations)
-    B_optimized, C_optimized = optimizer.get_factorization(gram_encoder)
-    return B_optimized, C_optimized
+    C_optimized = optimizer.get_factorization(gram_encoder)
+    return C_optimized
 
 
 def get_optimal_factorization_gram(
@@ -206,15 +212,15 @@ def get_optimal_factorization_gram(
     nb_steps: int,
     nb_epochs: int,
     optimizer_iterations: int = 10000,
-):
+) -> np.ndarray:
     workload = np.linalg.cholesky(_permute_lower_triangle(gram_matrix))
     optimizer = MatrixFactorizer(
         workload_matrix=workload,
         nb_epochs=nb_epochs,
     )
     gram_encoder_C = optimizer.optimize(optimizer_iterations)
-    B_optimized, C_optimized = optimizer.get_factorization(gram_encoder_C)
-    return B_optimized, C_optimized
+    C_optimized = optimizer.get_factorization(gram_encoder_C)
+    return C_optimized
 
 
 if __name__ == "__main__":
@@ -230,10 +236,11 @@ if __name__ == "__main__":
 
     workload = np.tri(nb_steps)
 
-    B_optimized, C_optimized = get_optimal_factorization(
+    C_optimized = get_optimal_factorization(
         workload=workload, nb_steps=nb_steps, nb_epochs=nb_epochs
     )
 
+    B_optimized = workload @ np.linalg.pinv(C_optimized)
     assert np.allclose(workload, B_optimized @ C_optimized)
 
     plt.figure()
@@ -244,16 +251,17 @@ if __name__ == "__main__":
 
     X = C_optimized.T @ C_optimized
 
-    B_optimized_gram, C_optimized_gram = get_optimal_factorization_gram(
+    C_optimized_gram = get_optimal_factorization_gram(
         gram_matrix=workload.T @ workload,
         nb_steps=nb_steps,
         nb_epochs=nb_epochs,
     )
 
+    B_optimized_gram = workload @ np.linalg.pinv(C_optimized_gram)
+
     assert np.allclose(workload, B_optimized_gram @ C_optimized_gram)
 
     plt.figure()
-    # C_optimized = np.linalg.pinv(C_optimized)
     plt.imshow(C_optimized_gram, cmap="bwr")
     plt.clim(-np.abs(C_optimized_gram).max(), np.abs(C_optimized_gram).max())
     plt.colorbar()

simulations/utils.py
@@ -1,5 +1,6 @@
 import time
 import tracemalloc
+from typing import Literal
 
 import networkx as nx
 import numpy as np
@@ -15,8 +16,22 @@ def expander_graph(n, d):
     return G
 
 
-def get_graph(name: str, n: int) -> nx.Graph:
+GraphName = Literal[
+    "expander",
+    "empty",
+    "cycle",
+    "complete",
+    "erdos",
+    "grid",
+    "star",
+    "florentine",
+    "ego",
+]
+
+
+def get_graph(name: GraphName, n: int, seed) -> nx.Graph:
     G: nx.Graph
+    assert n >= 0, f"Should not make star graph with {n} nodes"
     match name:
         case "expander":
             if n == 1:
@@ -43,12 +58,63 @@ def get_graph(name: str, n: int) -> nx.Graph:
             G = nx.cycle_graph(n)
         case "complete":
             G = nx.complete_graph(n)
+        case "erdos":
+            G = nx.erdos_renyi_graph(n, np.log(n) / n, seed=seed)
+        case "grid":
+            if int(np.sqrt(n)) ** 2 != n:
+                raise ValueError(
+                    f"Grid graph requires n to be a perfect square, got n={n}"
+                )
+            side = int(np.sqrt(n))
+            G = nx.grid_2d_graph(side, side)
+            G = nx.convert_node_labels_to_integers(G)
+        case "star":
+            if n == 1:
+                G = nx.empty_graph(n)
+            else:
+                G = nx.star_graph(n - 1)
+        case "florentine":
+            G = nx.florentine_families_graph()
+            # Convert indexes to int for florentine graph
+            # print(f"Number of nodes: {G.number_of_nodes()}")
+            G = nx.convert_node_labels_to_integers(G)
+            # print(f"Number of nodes: {G.number_of_nodes()}")
+        case "ego":
+            name_edgelist = "graphs/facebook/" + str(414) + ".edges"
+            my_graph = nx.read_edgelist(name_edgelist)
+            my_graph = nx.relabel_nodes(my_graph, lambda x: int(x))
+            Gcc = sorted(nx.connected_components(my_graph), key=len, reverse=True)
+            G = my_graph.subgraph(Gcc[0]).copy()
+            G = nx.convert_node_labels_to_integers(G, label_attribute="fb_id")
+            print(f"Nodes: {G.number_of_nodes()}")
         case _:
             raise ValueError(f"Invalid graph name {name}")
     G.add_edges_from(
         [(i, i) for i in range(G.number_of_nodes())]
     )  # Always keep this to make a useful graph
     return G
+
+
+def graph_require_seed(graph_name: GraphName) -> bool:
+    if graph_name in [
+        "star",
+        "grid",
+        "complete",
+        "cycle",
+        "empty",
+        "expander",
+        "florentine",
+        "ego",
+    ]:
+        return False
+    elif graph_name in [
+        "erdos",
+    ]:
+        return True
+    else:
+        raise NotImplementedError(
+            f"Did not implement wether graph {graph_name} requires a seed"
+        )
 
 
 def get_orthogonal_mask(n: int, epochs: int = 1) -> np.ndarray:

simulations/workloads_generator.py
@@ -1,9 +1,11 @@
+import os
+
 import numpy as np
 import optimal_factorization
 import torch
 import utils
 from scipy.linalg import toeplitz
-from utils import profile_memory_usage
+from utils import graph_require_seed, profile_memory_usage
 
 
 def get_pi(nb_nodes, nb_iterations):
@@ -118,7 +120,7 @@ def MF_ANTIPGD(nb_nodes, nb_iterations):
     return C_global
 
 
-@profile_memory_usage
+# @profile_memory_usage
 def build_DL_workload(
     matrix: np.ndarray, nb_steps: int, initial_power: int = 0
 ) -> np.ndarray:
@@ -145,7 +147,7 @@ def build_DL_workload(
     return time_matrix
 
 
-@profile_memory_usage
+# @profile_memory_usage
 def build_local_DL_gram_workload(
     matrix: np.ndarray, nb_steps: int, initial_power: int = 0
 ):
@@ -176,7 +178,15 @@ def build_local_DL_gram_workload(
     return gram_workload
 
 
-def build_local_DL_workload(matrix: np.ndarray, nb_steps: int, initial_power: int = 0):
+def build_local_DL_workload(
+    matrix: np.ndarray,
+    nb_steps: int,
+    initial_power: int = 0,
+    graph_name=None,
+    seed=None,
+    caching=True,
+    verbose=False,
+):
     """Builds the local version of the DL workload, where each node will have the same local correlation.
     This is the workload that will be optimized in order to obtain an optimal C such that A = B @ pi@ np.kron(In, C)
 
@@ -186,7 +196,27 @@ def build_local_DL_workload(matrix: np.ndarray, nb_steps: int, initial_power: in
         initial_power (int, default 0): Initial power of the workload matrix.
             Defines what power of matrix is in the diagonal.
             1 is the matrix itself (optimization workload), 0 is Id (privacy workload).
+        graph_name (optional str): only used for caching, name of the graph
+        seed (optional int): seed to generate the graph, only used for caching.
     """
+    nb_nodes = len(matrix)
+    cache_dir = f"cache/workloads/surrogate_workload/{graph_name}/nodes{nb_nodes}/steps{nb_steps}/"
+    cache_filename = f"local_DL_diagonalpower{initial_power}"
+
+    if graph_name is None:
+        caching = False
+    elif graph_require_seed(graph_name):
+        if seed is None:
+            caching = False
+        else:
+            cache_filename += f"_seed{seed}"
+
+    if caching:
+        cache_result = get_from_cache(
+            cache_dir=cache_dir, filename=cache_filename, verbose=verbose
+        )
+        if cache_result is not None:
+            return cache_result
 
     gram_workload = build_local_DL_gram_workload(
         matrix=matrix, nb_steps=nb_steps, initial_power=initial_power
@@ -202,45 +232,152 @@ def build_local_DL_workload(matrix: np.ndarray, nb_steps: int, initial_power: in
         surrogate_workload = np.linalg.cholesky(gram_workload_permuted)
     else:
         raise NotImplementedError("Non-positive definite Gram workload")
-        print(
-            "!" * 50
-            + "Warning - DL surrogate workload is not definite positive, trying an experimental surrogate workload that may make optimization fail."
-            + "!" * 50
+    if caching:
+        save_to_cache(
+            cache_dir=cache_dir,
+            filename=cache_filename,
+            matrix=surrogate_workload,
+            verbose=verbose,
         )
-        # Compute square root using - but problem if this workload is not full rank?
-        # TODO: remove this code, as the result may not be lower triangular? Not sure how the optimization will handle that.
-        eigvals, eigvecs = np.linalg.eigh(gram_workload)
-        # Set negative eigenvalues to zero (for numerical stability)
-        eigvals[eigvals < 0] = 0
-        surrogate_workload = eigvecs @ np.diag(np.sqrt(eigvals))
-
     return surrogate_workload
 
 
+def get_from_cache(cache_dir, filename, verbose=False):
+    cache_path = os.path.join(cache_dir, filename + ".npy")
+    # Try to load from cache
+    if os.path.exists(cache_path):
+        if verbose:
+            print(f"Loading from cache {cache_path}")
+        with open(cache_path, "rb") as f:
+            C_optimal = np.load(f)
+        return C_optimal
+    return None
+
+
+def save_to_cache(cache_dir, filename, matrix: np.ndarray, verbose=False):
+    cache_path = os.path.join(cache_dir, filename + ".npy")
+    os.makedirs(cache_dir, exist_ok=True)
+    with open(cache_path, "wb") as f:
+        np.save(f, matrix)
+    if verbose:
+        size_mb = os.path.getsize(cache_path) / (1024 * 1024)
+        print(f"Saved {size_mb:.2f} MB to cache {cache_path} ")
+
+
 def MF_OPTIMAL_DL(
-    communication_matrix, nb_nodes, nb_steps, nb_epochs, post_average=False
-):
-    # Initial_power dictates wether we start from I in the diagonal or W.
-    surrogate_workload = build_local_DL_workload(
-        communication_matrix, nb_steps=nb_steps, initial_power=int(post_average)
-    )
-    B_optimal, C_optimal = optimal_factorization.get_optimal_factorization(
-        surrogate_workload, nb_steps=nb_steps, nb_epochs=nb_epochs
-    )
-    if post_average:
-        surrogate_workload = build_local_DL_workload(
-            communication_matrix, nb_steps=nb_steps, initial_power=0
+    communication_matrix,
+    nb_nodes,
+    nb_steps,
+    nb_epochs,
+    post_average=False,
+    graph_name=None,
+    seed=None,
+    caching=True,
+    verbose=False,
+) -> np.ndarray:
+    """
+    Lazy loader for optimal DL factorization. Caches results to disk for repeated calls.
+
+    Args:
+        communication_matrix: np.ndarray, the communication matrix.
+        nb_nodes: int, number of nodes.
+        nb_steps: int, number of steps.
+        nb_epochs: int, number of epochs.
+        post_average: bool, whether to use post-averaging.
+        graph_name: str, unique name for the graph (required for caching).
+        seed: int, optional seed for uniqueness.
+        caching: bool, optional, default to True: wether to try and save intermediate workloads to cache.
+
+    Returns:
+        C_optimal
+    """
+    cache_dir = f"cache/correlations_matrixes/{graph_name}/nodes{nb_nodes}/steps{nb_steps}/epochs{nb_epochs}/"
+    cache_filename = f"OptimalDL_{'PostAVG' if post_average else "Msg"}"
+
+    if graph_name is None:
+        caching = False
+    elif graph_require_seed(graph_name):
+        if seed is None:
+            caching = False
+        else:
+            cache_filename += f"_seed{seed}"
+
+    if caching:
+        cache_result = get_from_cache(
+            cache_dir=cache_dir, filename=cache_filename, verbose=verbose
         )
-        B_optimal = surrogate_workload @ np.linalg.pinv(C_optimal)
-    return B_optimal, C_optimal
+        if cache_result is not None:
+            return cache_result
 
-
-def MF_OPTIMAL_local(communication_matrix, nb_nodes, nb_steps, nb_epochs):
-    centralized_workload = np.tri(nb_steps)
-    B_optimal, C_optimal = optimal_factorization.get_optimal_factorization(
-        centralized_workload, nb_steps=nb_steps, nb_epochs=nb_epochs
+    surrogate_workload = build_local_DL_workload(
+        communication_matrix,
+        nb_steps=nb_steps,
+        initial_power=int(post_average),
+        graph_name=graph_name,
+        seed=seed,
+        verbose=verbose,
     )
-    return B_optimal, C_optimal
+    C_optimal = optimal_factorization.get_optimal_factorization(
+        surrogate_workload, nb_steps=nb_steps, nb_epochs=nb_epochs, verbose=verbose
+    )
+
+    # Save to cache
+    if caching:
+        save_to_cache(
+            cache_dir=cache_dir,
+            filename=cache_filename,
+            matrix=C_optimal,
+            verbose=verbose,
+        )
+
+    return C_optimal
+
+
+def MF_OPTIMAL_local(
+    communication_matrix,
+    nb_nodes,
+    nb_steps,
+    nb_epochs,
+    caching=True,
+    verbose=False,
+):
+    """
+    Computes and caches the optimal local factorization.
+
+    Args:
+        communication_matrix: np.ndarray, the communication matrix (unused here).
+        nb_nodes: int, number of nodes.
+        nb_steps: int, number of steps.
+        nb_epochs: int, number of epochs.
+        caching: bool, optional, default to True.
+
+    Returns:
+        C_optimal: np.ndarray, the optimal factorization matrix.
+    """
+    cache_dir = f"cache/centralized/steps{nb_steps}/epochs{nb_epochs}"
+    cache_filename = f"optimal_local"
+
+    if caching:
+        cache_result = get_from_cache(
+            cache_dir=cache_dir, filename=cache_filename, verbose=verbose
+        )
+        if cache_result is not None:
+            return cache_result
+
+    centralized_workload = np.tri(nb_steps)
+    C_optimal = optimal_factorization.get_optimal_factorization(
+        centralized_workload, nb_steps=nb_steps, nb_epochs=nb_epochs, verbose=verbose
+    )
+
+    if caching:
+        save_to_cache(
+            cache_dir=cache_dir,
+            filename=cache_filename,
+            matrix=C_optimal,
+            verbose=verbose,
+        )
+
+    return C_optimal
 
 
 def space_to_time_permutation_matrix(nb_steps: int, nb_nodes: int):